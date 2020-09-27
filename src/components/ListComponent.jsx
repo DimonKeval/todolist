21b67f5b7e65f3a6498b82ba08cd8632f8f281bd
Petro Manus <petrmanus@gmail.com>
2020-09-27 10:51:30 +0200

    adding arrayList in ListComponent.jsx

--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+const arrayList = [
+    {task: 'Prepare breakfast'},
+    {task: 'Take kids to school'},
+    {task: 'Go to the office'},
+    {task: 'Go to the grocery store'},
+    {task: 'Take kids from school'},
+    {task: 'Prepare dinner'},
+    {task: 'Go to sleep'},
+];
+
+function renderTask(el, index){
+    return <div>
+    <p>{el.task}</p>
+    </div>
+}
+
+
 export function ListComponent(props){
+    return
+    {arrayList.map((el, index)=>
+    renderTask(index, el))}
 
 }
+
