328f2e1db69c70823e6984e4a1acf19442b4066c
DimonKeval <kevaldima@gmail.com>
2020-09-27 11:51:24 +0200

    technical commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import {TaskButton} from './components/TaskButton.jsx'
+import {InputComponent} from "./components/InputComponent";
 import {ListComponent} from "./components/ListComponent";
 
 export function App() {
-  return <>
+  return (<>
       <p>Hello! I'm ToDoList!</p>
-      <TaskButton text='Add a new task to your life!'/>
-
-      <ListComponent />
-
-      </>;
+      <TaskButton text='Add a new task to your life!' id='taskButton'/>
+      <InputComponent/>
+      <ListComponent/>
+      </>
+  );
 }
 
 export default App;
